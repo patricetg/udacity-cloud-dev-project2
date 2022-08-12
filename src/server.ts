46e2259b0a1504b391bee67fc2544d71a0707de5
Ekou LOGOSU-TEKO <patricelogosuteko@gmail.com>
2022-08-12 09:33:35 +0000

    image filtering implemented

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import {Request, Response} from "express";
 
 (async () => {
 
@@ -18,17 +19,35 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // endpoint to filter an image from a public url.
   // IT SHOULD
   //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
+  //    1. validate the image_url query : done
+  //    2. call filterImageFromURL(image_url) to filter the image : done
+  //    3. send the resulting file in the response : done
+  //    4. deletes any files on the server on finish of the response : done
   // QUERY PARAMATERS
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+  app.get("/filteredimage", async (req:Request, resp:Response)=>{
+    
+    //return resp.send(req.query);
 
+    let imageUrl:string =req.query.image_url;
+    if(!imageUrl)
+      return resp.status(400)
+                 .send("image_url query string parameter is required!");
+
+    //return resp.send(imageUrl);
+
+    let filteredPath:string = await filterImageFromURL(imageUrl);
+
+    //return resp.send(filteredPath);
+    return resp.sendFile(filteredPath,(err)=>{
+        deleteLocalFiles([filteredPath]);
+    });
+    
+  });
   //! END @TODO1
   
   // Root Endpoint
